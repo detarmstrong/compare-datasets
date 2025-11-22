a4f23e59ec0935951f98e99f712474ebb07a5f26
Danny Armstrong <darmstrong@visiontree.com>
2025-11-22 14:17:26 -0800

    fixes logo

--- src/DragNDropForm.tsx
+++ src/DragNDropForm.tsx
@@ -154,17 +154,21 @@ function Form(props: FormProps) {
         label="Upload or drop two .csv files here"
         children={stack2}
       />
-      <div style={{ 
-        textAlign: 'left', 
-        maxWidth: '400px',
-        margin: '40px auto 0'
-      }}>
+      <div
+        style={{
+          textAlign: 'left',
+          maxWidth: '400px',
+          margin: '40px auto 0',
+        }}
+      >
         <Typography variant="h5" gutterBottom sx={{ fontWeight: 'bold' }}>
           Compare datasets
         </Typography>
         <Typography variant="body1" sx={{ lineHeight: 1.8 }}>
-          1. Upload two datasets<br />
-          2. Join datasets by key<br />
+          1. Upload two datasets
+          <br />
+          2. Join datasets by key
+          <br />
           3. Compare - view discrepancies and similarities
         </Typography>
       </div>
